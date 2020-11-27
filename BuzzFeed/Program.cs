effb598b499018464255627c57e23e681dc10b6a
Daniel <daniel.andersson@iths.se>
2020-11-27 09:48:44 +0100

    done

--- BuzzFeed/Program.cs
+++ BuzzFeed/Program.cs
@@ -2,64 +2,61 @@
 
 namespace BuzzFeed
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            FizzBuzz fizzbuzz = new FizzBuzz();
-            Console.WriteLine("enter a number between 1 to 300");
-            fizzbuzz.PrintFizzBuzz(44);
-            //kolla under 300
-            //köra
-
-            Console.ReadKey();
-        }
-    }
-    public class FizzBuzz
-    {
-        public void PrintFizzBuzz(int num)
-        {
-            for (int i = 1; i <= num; i++)
-            {
-                Console.WriteLine(GetFizzBuzzString(i));
-            }
-        }
-        public int CheckInput(string numInput)
-        {
-            int num;
-            var parseSuccess = int.TryParse(numInput, out num);
-            if (num >= 1 && num <= 300 && parseSuccess)
-            {
-                return num;
-            }
-            else
-            {
-                Console.WriteLine("You entered the wrong number");
-                return 0;
-            }
-        }
-        public string GetFizzBuzzString(int num)
-        {
-            if (num == 42)
-            {
-                return "Answer to the Ultimate Question of Life, the Universe, and Everything";
-            }
-            else if (num % 3 == 0 && num % 5 == 0)
-            {
-                return "Fizzbuzz";
-            }
-            else if (num % 5 == 0)
-            {
-                return "Buzz";
-            }
-            else if (num % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else
-            {
-                return num.ToString();
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FizzBuzz fizzbuzz = new FizzBuzz();
+			Console.WriteLine("enter a number between 1 to 300");
+			fizzbuzz.PrintFizzBuzz(fizzbuzz.CheckInput(Console.ReadLine()));
+			Console.ReadKey();
+		}
+	}
+	public class FizzBuzz
+	{
+		public void PrintFizzBuzz(int num)
+		{
+			for (int i = 1; i <= num; i++)
+			{
+				Console.WriteLine(GetFizzBuzzString(i));
+			}
+		}
+		public int CheckInput(string numInput)
+		{
+			int num;
+			var parseSuccess = int.TryParse(numInput, out num);
+			if (num >= 1 && num <= 300 && parseSuccess)
+			{
+				return num;
+			}
+			else
+			{
+				Console.WriteLine("You entered the wrong number");
+				return 0;
+			}
+		}
+		public string GetFizzBuzzString(int num)
+		{
+			if (num == 42)
+			{
+				return "Answer to the Ultimate Question of Life, the Universe, and Everything";
+			}
+			else if (num % 3 == 0 && num % 5 == 0)
+			{
+				return "Fizzbuzz";
+			}
+			else if (num % 5 == 0)
+			{
+				return "Buzz";
+			}
+			else if (num % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else
+			{
+				return num.ToString();
+			}
+		}
+	}
 }
--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -7,74 +7,74 @@ using System.Linq;
 
 namespace FizzBuzzTest
 {
-    [TestClass]
-    public class FizzBuzzTest
-    {
-        [TestMethod]
-        public void PrintFizzBuzzTest()
-        {
-            //arrange
-            using (StringWriter sw = new StringWriter()) 
-            {
-                FizzBuzz fizzBuzz = new FizzBuzz();
-                var stringBuilder = sw.GetStringBuilder();
-                Console.SetOut(sw);
-                //Act
-                fizzBuzz.PrintFizzBuzz(44);
-                var consoleText = sw.ToString();
-                var lines = String.Join(Environment.NewLine, consoleText);
-                var expected = 44;
-                var actual = lines.Length;
-                Assert.AreEqual(expected, lines);
-                //Assert
-            }
-            
-        }
-        [TestMethod]
-        public void CheckInputTest()
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            Assert.AreEqual(5, fizzBuzz.CheckInput("5"));
-            Assert.AreEqual(0, fizzBuzz.CheckInput("k"));
-            Assert.AreEqual(0, fizzBuzz.CheckInput("305"));
-            Assert.AreEqual(0, fizzBuzz.CheckInput("-1"));
-            Assert.AreEqual(0, fizzBuzz.CheckInput("0"));
-            Assert.AreEqual(42, fizzBuzz.CheckInput("42"));
-            Assert.AreEqual(90, fizzBuzz.CheckInput("90"));
-        }
-        [TestMethod]
-        public void GetInputTest()
-        {
-            
-        }
-        [TestMethod]
-        public void GetFizzBuzzStringTest()
-        {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            for (int i = 1; i <= 300; i++)
-            {
-                var text = fizzBuzz.GetFizzBuzzString(i);
-                if (i == 42)
-                {
-                    Assert.AreEqual("Answer to the Ultimate Question of Life, the Universe, and Everything", text);
-                }
-                else if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Assert.AreEqual("Fizzbuzz", text);
-                }
-                else if (i % 5 == 0)
-                {
-                    Assert.AreEqual("Buzz", text);
-                }
-                else if (i % 3 == 0)
-                {
-                    Assert.AreEqual("Fizz", text);
-                }
-                else
-                {
-                    Assert.AreEqual(i.ToString(), text);
-                }
-            }
-        }
-    }
+	[TestClass]
+	public class FizzBuzzTest
+	{
+		[TestMethod]
+		public void PrintFizzBuzzTest()
+		{
+			//arrange
+			using (StringWriter sw = new StringWriter())
+			{
+				FizzBuzz fizzBuzz = new FizzBuzz();
+				var stringBuilder = sw.GetStringBuilder();
+				Console.SetOut(sw);
+				//Act
+				fizzBuzz.PrintFizzBuzz(10);
+				var consoleText = sw.ToString();
+				var lines = String.Join(Environment.NewLine, consoleText);
+				var expected = 10;
+				var actual = lines.Split('\n').Length -1;
+				//Assert
+				Assert.AreEqual(expected, actual);
+			}
+
+		}
+		[TestMethod]
+		public void CheckInputTest()
+		{
+			FizzBuzz fizzBuzz = new FizzBuzz();
+			Assert.AreEqual(5, fizzBuzz.CheckInput("5"));
+			Assert.AreEqual(0, fizzBuzz.CheckInput("k"));
+			Assert.AreEqual(0, fizzBuzz.CheckInput("305"));
+			Assert.AreEqual(0, fizzBuzz.CheckInput("-1"));
+			Assert.AreEqual(0, fizzBuzz.CheckInput("0"));
+			Assert.AreEqual(42, fizzBuzz.CheckInput("42"));
+			Assert.AreEqual(90, fizzBuzz.CheckInput("90"));
+		}
+		[TestMethod]
+		public void GetInputTest()
+		{
+
+		}
+		[TestMethod]
+		public void GetFizzBuzzStringTest()
+		{
+			FizzBuzz fizzBuzz = new FizzBuzz();
+			for (int i = 1; i <= 300; i++)
+			{
+				var text = fizzBuzz.GetFizzBuzzString(i);
+				if (i == 42)
+				{
+					Assert.AreEqual("Answer to the Ultimate Question of Life, the Universe, and Everything", text);
+				}
+				else if (i % 3 == 0 && i % 5 == 0)
+				{
+					Assert.AreEqual("Fizzbuzz", text);
+				}
+				else if (i % 5 == 0)
+				{
+					Assert.AreEqual("Buzz", text);
+				}
+				else if (i % 3 == 0)
+				{
+					Assert.AreEqual("Fizz", text);
+				}
+				else
+				{
+					Assert.AreEqual(i.ToString(), text);
+				}
+			}
+		}
+	}
 }
