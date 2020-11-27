24a04db82ce8970e04a63e4ff1a3d41b48f570b4
emil.martini <emil.martini@contica.se>
2020-11-27 11:18:45 +0100

    checked code coverage and added comments

--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -19,12 +19,14 @@ namespace FizzBuzzTest
 				FizzBuzz fizzBuzz = new FizzBuzz();
 				var stringBuilder = sw.GetStringBuilder();
 				Console.SetOut(sw);
+
 				//Act
 				fizzBuzz.PrintFizzBuzz(150);
 				var consoleText = sw.ToString();
 				var lines = String.Join(Environment.NewLine, consoleText);
 				var expected = 150;
 				var actual = lines.Split(Environment.NewLine).Length -1;
+
 				//Assert
 				Assert.AreEqual(expected, actual);
 			}
@@ -33,7 +35,10 @@ namespace FizzBuzzTest
 		[TestMethod]
 		public void CheckInputTest()
 		{
+			//Arrange
 			FizzBuzz fizzBuzz = new FizzBuzz();
+
+			//Asser
 			Assert.AreEqual(5, fizzBuzz.CheckInput("5"));
 			Assert.AreEqual(0, fizzBuzz.CheckInput("k"));
 			Assert.AreEqual(0, fizzBuzz.CheckInput("305"));
@@ -42,18 +47,19 @@ namespace FizzBuzzTest
 			Assert.AreEqual(42, fizzBuzz.CheckInput("42"));
 			Assert.AreEqual(90, fizzBuzz.CheckInput("90"));
 		}
-		[TestMethod]
-		public void GetInputTest()
-		{
 
-		}
 		[TestMethod]
 		public void GetFizzBuzzStringTest()
 		{
+			//Arrange
 			FizzBuzz fizzBuzz = new FizzBuzz();
+
+			//Act
 			for (int i = 1; i <= 300; i++)
 			{
 				var text = fizzBuzz.GetFizzBuzzString(i);
+
+				//Assert
 				if (i == 42)
 				{
 					Assert.AreEqual("Answer to the Ultimate Question of Life, the Universe, and Everything", text);
